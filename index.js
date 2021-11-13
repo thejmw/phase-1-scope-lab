a6e494cbb3b6afb392593151b783d2bb4f25ab3d
Jason <thejasonmichaelwilliams@gmail.com>
2021-11-13 10:56:24 -0500

    commit tree 66029a6f35c26e8086abc7d46d5ea5f2df3fab36

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase();
+}
+
+var bestCustomer;
+
+function setBestCustomer(){
+    bestCustomer = 'not bob'
+}
+
+function overwriteBestCustomer(){
+    return bestCustomer = 'maybe bob'
+}
+
+const leastFavoriteCustomer = "michael";
+function changeLeastFavoriteCustomer(){
+   leastFavoriteCustomer = "anything"
+   return leastFavoriteCustomer
+}
